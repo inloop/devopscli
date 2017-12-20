a91283feb0453e426c0c2befcabd17a2a693a0cf
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2017-12-20 20:34:16 +0100

    Fix lambda deploy cmd

--- cmd/aws.go
+++ cmd/aws.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"errors"
 
-	"github.com/inloop/devopscli/tools"
 	"github.com/inloop/goclitools"
 	"github.com/urfave/cli"
 )
@@ -23,21 +22,8 @@ func AWSLambdaCmd() cli.Command {
 	return cli.Command{
 		Name:  "lambda",
 		Flags: []cli.Flag{},
-		Action: func(c *cli.Context) error {
-
-			host, err := tools.DockerAutodetectHost()
-
-			if err != nil {
-				return cli.NewExitError(err, 1)
-			}
-
-			if host != "" {
-				goclitools.Log("Found docker host at:", host)
-			} else {
-				goclitools.Log("Host docker not found but current configuration works without specifying DOCKER_HOST")
-			}
-
-			return nil
+		Subcommands: []cli.Command{
+			AWSLambdaDeployCmd(),
 		},
 	}
 
