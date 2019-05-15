3c466f9070f2b3d705d9b1bf70a0133405fa02ea
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2019-05-15 17:32:11 +0200

    Add docker get-host cmd

--- cmd/docker.go
+++ cmd/docker.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/inloop/devopscli/tools"
@@ -14,6 +15,7 @@ func DockerCmd() cli.Command {
 		Name: "docker",
 		Subcommands: []cli.Command{
 			DockerDetectHostCmd(),
+			DockerGetHostCmd(),
 		},
 	}
 }
@@ -35,27 +37,57 @@ func DockerDetectHostCmd() cli.Command {
 	}
 }
 
+// DockerDetectHostCmd ...
+func DockerGetHostCmd() cli.Command {
+	return cli.Command{
+		Name:  "get-host",
+		Flags: []cli.Flag{},
+		Action: func(c *cli.Context) error {
+
+			host, err := DockerDetectHost()
+
+			if err != nil {
+				return cli.NewExitError(err, 1)
+			} else {
+				fmt.Print(host)
+			}
+
+			return nil
+		},
+	}
+}
+
 // DockerDetectHostAndUpdateEnv ...
-func DockerDetectHostAndUpdateEnv() error {
+func DockerDetectHost() (string, error) {
 
 	dockerHost := os.Getenv("DOCKER_HOST")
 
 	if !tools.DockerCheckHost(dockerHost) {
-		goclitools.Log("Current DOCKER_HOST(" + dockerHost + ") seems to not work properly!")
 		dockerHost = ""
 	}
 
 	if dockerHost == "" {
-		goclitools.Log("Trying to find another docker host...")
 		host, err := tools.DockerAutodetectHost()
 		if err != nil {
-			return cli.NewExitError(err, 1)
+			return dockerHost, err
 		}
-		goclitools.Log("New working docker host found:", host)
 		dockerHost = host
 	}
 
+	return dockerHost, nil
+}
+
+// DockerDetectHostAndUpdateEnv ...
+func DockerDetectHostAndUpdateEnv() error {
+
+	dockerHost, err := DockerDetectHost()
+
+	if err != nil {
+		return err
+	}
+
 	if dockerHost != "" {
+		goclitools.Log("Working docker host found:", dockerHost)
 		os.Setenv("DOCKER_HOST", dockerHost)
 	}
 	return nil
